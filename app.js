e5963991d567d6a47c211b57426e15ad5ca89acc
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-09-19 08:46:24 -0300

    k

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log('Olá mundo');
+
+
+function criaCartao(categoria, subcategoria, pergunta, resposta, exemplo) {
+
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    
+    <div class="conteudo-cartao">
+
+    <button>${categoria}</button>
+    <button>${subcategoria}</button>
+
+    <div class="pergunta-cartao">
+        <p>${pergunta}</p>
+    </div>
+
+    <div class="resposta-cartao"></div>
+        <p>${resposta}</p>
+    </div>
+
+    <div class="exemplo-cartao"></div>
+        <p>${exemplo}</p>
+    </div>
+    
+    </div>
+    
+    `;
+}
